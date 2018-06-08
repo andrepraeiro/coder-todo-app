f943c68bfff3048114bd6129ad0bf9e4966daa00
Andre Praeiro <andrepreiro@gmail.com>
2018-06-07 21:59:44 -0400

    finishing class #60-1

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -14,7 +14,7 @@ export default class Todo extends Component {
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.handleRemove = this.handleRemove.bind(this)
-        this.handleClear=this.handleClear.bind(this)
+        this.handleClear = this.handleClear.bind(this)
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
         this.handleSearch = this.handleSearch.bind(this)
