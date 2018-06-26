078e5c2ac7549ee6b959c16521dc7a2af479cae4
Andre Praeiro <andrepreiro@gmail.com>
2018-06-25 22:06:45 -0400

    finishing lecture #77 - action creattor search

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -15,10 +15,12 @@ const INITIAL_STATE = {
     }]
 }
 
-export default (state = INITIAL_STATE, action) => {    
+export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'DESCRIPTION_CHANGED':
             return { ...state, description: action.payload }
+        case 'TODO_SEARCHED':
+            return { ...state, list: action.payload.data }
         default:
             return state
     }
